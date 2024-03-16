f962be50fa4fe35d48b2de95c398cb945dc0cde5
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-03-16 19:28:59 +0100

    add link header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,14 @@
 <header>
-    header
+    <ul>
+        <li><a href="{{route('characters')}}">characters</a></li>
+        <li><a href="{{route('comics')}}">COMICS</a></li>
+        <li><a href="{{route('movies')}}">MOVIES</a></li>
+        <li><a href="{{route('tv')}}">TV</a></li>
+        <li><a href="{{route('games')}}">games</a></li>
+        <li><a href="{{route('collectibles')}}">collectibles</a></li>
+        <li><a href="{{route('videos')}}">videos</a></li>
+        <li><a href="{{route('fans')}}">fans</a></li>
+        <li><a href="{{route('news')}}">news</a></li>
+
+    </ul>
 </header>
