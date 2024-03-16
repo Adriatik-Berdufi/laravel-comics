69c1a61a787bd88e17ce625b17e57fe8f771d898
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-03-16 18:25:46 +0100

    add partilas header and footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,16 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="ie=edge" http-equiv="X-UA-Compatible">
-    <title>Document</title>
+    <title>Comics</title>
 
     @vite('resources/js/app.js')
   </head>
 
   <body>
-    <h1>Laravel with Bootstrap + Vite</h1>
+    @include('partials.header')
+    <main>main</main>
+    @include('partials.footer')
+
   </body>
 
 </html>
